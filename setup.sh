62eadf0ed84b9d86fa864446d79940b9539f5627
Koshin S Hegde <kosh@kosh-web.cfd>
2026-04-09 14:29:21 +0530

    init

--- setup.sh
+++ setup.sh
@@ -10,15 +10,15 @@ sleep 1
 echo "installing dependencies"
 apt update
 apt install -y python3 python3-venv curl zstd git
-echo "installing ollama"
-curl -fsSL https://ollama.com/install.sh | sh
-echo "starting ollama"
-service ollama start
 echo "Creating python3 environment"
 python3 -m venv venv
 source venv/bin/activate
 echo "Installing demo program dependencies"
-pip install -r requirements-demo.txt
+pip3 install -r requirements-demo.txt
+echo "installing ollama (This will take a lot of time)"
+curl -fsSL https://ollama.com/install.sh | sh
+echo "starting ollama"
+service ollama start
 echo "Pulling ollama demo embedding model (this will take a lot of time)"
 ollama pull nomic-embed-text
 echo "Downloading the project with demo"
